77589ad69345847b9f6c90b025c955b41be28430
ianraphael <labicaniian@gmail.com>
2023-01-23 22:59:03 +0800

    Initial commit.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutComponent } from './components/about/about.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ComposeComponent } from './components/compose/compose.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { BlogComponent } from './components/blog/blog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    HomeComponent,
+    FooterComponent,
+    HeaderComponent,
+    ComposeComponent,
+    ContactComponent,
+    BlogComponent
   ],
   imports: [
     BrowserModule,
